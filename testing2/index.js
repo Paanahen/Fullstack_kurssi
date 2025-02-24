b892e9b64136d07569a260d14dc09c14b72d6775
Paanahen <henrisamulipaananen@gmail.com>
2025-02-24 14:39:58 +0200

    notes_1.0

--- testing2/index.js
+++ testing2/index.js
@@ -3,6 +3,7 @@ const app = express()
 app.use(express.json())
 const cors = require('cors')
 app.use(cors())
+app.use(express.static('dist'))
 
 let notes = [
   {
@@ -27,9 +28,9 @@ let notes = [
     important: true
   }
 ]
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
-})
+//app.get('/', (request, response) => {
+//  response.send('<h1>Hello World!</h1>')
+//})
 
 app.get('/api/notes/:id', (request, response)=>{
   const id = request.params.id
